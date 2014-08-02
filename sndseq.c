8425740b8d1f577177a5aa38e0598d651d4084c5
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2014-08-02 17:38:46 +0200

    added an empty event type

--- sndseq.c
+++ sndseq.c
@@ -32,6 +32,37 @@ static PyTypeObject PortType = {
 	"wrapper for snd_seq port",  /* tp_doc */
 };
 
+/* -- ALSA sequencer event -------------------------------------------------- */
+typedef struct {
+	PyObject_HEAD
+	snd_seq_event_t ev;
+} Event;
+
+static PyTypeObject EventType = {
+	PyObject_HEAD_INIT(NULL)
+	0,                             /*ob_size*/
+	"sndseq.Event",                /*tp_name*/
+	sizeof(Event),                 /*tp_basicsize*/
+	0,                             /*tp_itemsize*/
+	0,                             /*tp_dealloc*/
+	0,                             /*tp_print*/
+	0,                             /*tp_getattr*/
+	0,                             /*tp_setattr*/
+	0,                             /*tp_compare*/
+	0,                             /*tp_repr*/
+	0,                             /*tp_as_number*/
+	0,                             /*tp_as_sequence*/
+	0,                             /*tp_as_mapping*/
+	0,                             /*tp_hash */
+	0,                             /*tp_call*/
+	0,                             /*tp_str*/
+	0,                             /*tp_getattro*/
+	0,                             /*tp_setattro*/
+	0,                             /*tp_as_buffer*/
+	Py_TPFLAGS_DEFAULT,            /*tp_flags*/
+	"wrapper for snd_seq_event_t", /* tp_doc */
+};
+
 /* -- Python module --------------------------------------------------------- */
 static PyMethodDef module_methods[] = {
 	{NULL}  /* Sentinel */
@@ -44,15 +75,20 @@ PyMODINIT_FUNC
 initsndseq(void) 
 {
 	PyObject* m;
-
-	// init port
-	PortType.tp_new = PyType_GenericNew;
-	if (PyType_Ready(&PortType) < 0)
-		return;
-
 	m = Py_InitModule3("sndseq", module_methods,
 		"wrapper for the alsa sequencer");
 
+	// Port
+	PortType.tp_new = PyType_GenericNew;
+	if (PyType_Ready(&PortType) < 0)
+		return;
 	Py_INCREF(&PortType);
 	PyModule_AddObject(m, "Port", (PyObject *)&PortType);
+
+	// Event
+	EventType.tp_new = PyType_GenericNew;
+	if (PyType_Ready(&EventType) < 0)
+		return;
+	Py_INCREF(&EventType);
+	PyModule_AddObject(m, "Event", (PyObject *)&EventType);
 }
